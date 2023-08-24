200d262c282297c63e93e103c1ed8b38cff46fde
uroNana <mirianamartinelli97@libero.it>
2023-08-24 15:57:42 +0200

    object with fun getWeatherDataList with return ListOf(manually compiled) updated; TODO (remove comments below when home_recycler_view and HomeListAdapter is created) created

--- app/src/main/java/co/develhope/meteoapp/HomeScreenFragment.kt
+++ app/src/main/java/co/develhope/meteoapp/HomeScreenFragment.kt
@@ -1,59 +1,60 @@
 package co.develhope.meteoapp
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.util.Locale
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeScreenFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeScreenFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_home_screen, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeScreenFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeScreenFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        object {
+            @RequiresApi(Build.VERSION_CODES.O)
+            fun getWeatherDataList(): List<HomeScreenList> {
+                val currentDate = LocalDate.now()
+                val dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+                val formattedDate = currentDate.format(dateFormatter)
+
+                val dayOfTheWeek = LocalDate.now().dayOfWeek
+                val dayOfWeekText = dayOfTheWeek.getDisplayName(TextStyle.FULL, Locale.getDefault())
+
+                return listOf(
+                    HomeScreenList(dayOfWeekText, formattedDate, 22, 30,
+                        MainActivity.weatherIcons.sun, 0, 10),
+                    HomeScreenList(dayOfWeekText, formattedDate, 20, 28,
+                        MainActivity.weatherIcons.rain, 60, 15),
+                    HomeScreenList(dayOfWeekText, formattedDate, 23, 31,
+                        MainActivity.weatherIcons.sun_cloud, 0, 12)
+
+                )
             }
+        }
+
+        //TODO remove comments below when home_recycler_view and HomeListAdapter is created
+       // val list = findViewById<RecyclerView>(R.id.home_recycler_view)
+       // val adapter = HomeListAdapter()
+       // list.adapter = adapter
+       // list.layoutManager = LinearLayoutManager(this)
+
+       // adapter.submitList(getWeatherDataList)
+
     }
 }
