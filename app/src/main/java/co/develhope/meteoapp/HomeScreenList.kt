6543b7da0095b96db44f3071a3d778feef8412d4
uroNana <mirianamartinelli97@libero.it>
2023-08-24 15:43:12 +0200

    HomeScreenList parameters updated

--- app/src/main/java/co/develhope/meteoapp/HomeScreenList.kt
+++ app/src/main/java/co/develhope/meteoapp/HomeScreenList.kt
@@ -1,3 +1,9 @@
 package co.develhope.meteoapp
 
-data class HomeScreenList()
+data class HomeScreenList(val dayOfWeek: String,
+                           val date: String,
+                           val minTemperature: Int,
+                           val maxTemperature: Int,
+                           val weatherIcon: MainActivity.weatherIcons,
+                           val precipitation: Int,
+                           val windSpeed: Int)
