e00f5ee38fd402921fef7dcf45ea045b5b9232b2
uroNana <mirianamartinelli97@libero.it>
2023-08-30 03:31:54 +0200

    binding updated in HomeScreenFragment; onDestroyView fun added in HomeScreenFragment; print line added in HomeScreenFragment

--- app/src/main/java/co/develhope/meteoapp/HomeScreenFragment.kt
+++ app/src/main/java/co/develhope/meteoapp/HomeScreenFragment.kt
@@ -2,25 +2,29 @@ package co.develhope.meteoapp
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import co.develhope.meteoapp.databinding.FragmentHomeScreenBinding
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.format.TextStyle
 import java.util.Locale
 
+@Suppress("UNREACHABLE_CODE")
 class HomeScreenFragment : Fragment() {
+    private var _binding: FragmentHomeScreenBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_home_screen, container, false)
+        _binding = FragmentHomeScreenBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -45,8 +49,10 @@ class HomeScreenFragment : Fragment() {
                         MainActivity.weatherIcons.sun_cloud, 0, 12)
 
                 )
+                Log.v("HomeScreenFragment","the list is: ${getWeatherDataList()}")
             }
         }
+
 
         //TODO remove comments below when HomeListAdapter is created
        // val list = findViewById<RecyclerView>(R.id.home_recycler_view)
@@ -56,5 +62,9 @@ class HomeScreenFragment : Fragment() {
 
        // adapter.submitList(getWeatherDataList)
 
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
