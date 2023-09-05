d049c5283a7d08ba3a77297bcb8fe81d9838b0a2
uroNana <mirianamartinelli97@libero.it>
2023-09-05 15:47:57 +0200

    object Data updated

--- app/src/main/java/co/develhope/meteoapp/Data.kt
+++ app/src/main/java/co/develhope/meteoapp/Data.kt
@@ -10,58 +10,63 @@ import java.util.Locale
 object Data {
     @RequiresApi(Build.VERSION_CODES.O)
     fun getWeatherDataList(): List<DailySummaryForecast> {
-        val currentDate = LocalDate.now()
-        val pattern = "dd/MM/yyyy"
+
+        val pattern = "dd/MM"
         val dateFormatter = DateTimeFormatter.ofPattern(pattern)
-        val formattedDate = currentDate.format(dateFormatter)
-        val dateOfYear = LocalDate.parse(formattedDate, dateFormatter)
+        val currentDate = LocalDate.now()
+        val dateOfMonthOne = LocalDate.parse(currentDate.format(dateFormatter), dateFormatter)
+        val dateOfMonthTwo = LocalDate.parse(currentDate.plusDays(1)
+            .format(dateFormatter), dateFormatter)
+        val dateOfMonthThree = LocalDate.parse(currentDate.plusDays(2)
+            .format(dateFormatter), dateFormatter)
+        val dateOfMonthFour = LocalDate.parse(currentDate.plusDays(3)
+            .format(dateFormatter), dateFormatter)
+        val dateOfMonthFive = LocalDate.parse(currentDate.plusDays(4)
+            .format(dateFormatter), dateFormatter)
+        val dateOfMonthSix = LocalDate.parse(currentDate.plusDays(5)
+            .format(dateFormatter), dateFormatter)
+        val dateOfMonthSeven = LocalDate.parse(currentDate.plusDays(6)
+            .format(dateFormatter), dateFormatter)
 
 
-        val todayData = LocalDate.now().dayOfWeek
-        val todayDateText = todayData.getDisplayName(TextStyle.FULL, Locale.ITALIAN)
+        val todayDateText = LocalDate.now().dayOfWeek.getDisplayName(TextStyle.FULL, Locale.ITALIAN)
             .uppercase(Locale.ITALIAN)
-        val tomorrowDate = LocalDate.now().plusDays(1).dayOfWeek
-        val tomorrowDateText = tomorrowDate.getDisplayName(TextStyle.FULL, Locale.ITALIAN)
-            .uppercase(Locale.ITALIAN)
-        val threeDaysNow = LocalDate.now().plusDays(2).dayOfWeek
-        val threeDaysNowText = threeDaysNow.getDisplayName(TextStyle.FULL, Locale.ITALIAN)
-            .uppercase(Locale.ITALIAN)
-        val fourDaysNow = LocalDate.now().plusDays(3).dayOfWeek
-        val fourDaysNowText = fourDaysNow.getDisplayName(TextStyle.FULL, Locale.ITALIAN)
-            .uppercase(Locale.ITALIAN)
-        val fiveDaysNow = LocalDate.now().plusDays(4).dayOfWeek
-        val fiveDaysNowText = fiveDaysNow.getDisplayName(TextStyle.FULL, Locale.ITALIAN)
-            .uppercase(Locale.ITALIAN)
-        val sixDaysNow = LocalDate.now().plusDays(5).dayOfWeek
-        val sixDaysNowText = sixDaysNow.getDisplayName(TextStyle.FULL, Locale.ITALIAN)
-            .uppercase(Locale.ITALIAN)
-        val sevenDaysNow = LocalDate.now().plusDays(6).dayOfWeek
-        val sevenDaysNowText = sevenDaysNow.getDisplayName(TextStyle.FULL, Locale.ITALIAN)
-            .uppercase(Locale.ITALIAN)
+        val tomorrowDateText = LocalDate.now().plusDays(1).dayOfWeek
+            .getDisplayName(TextStyle.FULL, Locale.ITALIAN).uppercase(Locale.ITALIAN)
+        val threeDaysNowText = LocalDate.now().plusDays(1).dayOfWeek
+            .getDisplayName(TextStyle.FULL, Locale.ITALIAN).uppercase(Locale.ITALIAN)
+        val fourDaysNowText = LocalDate.now().plusDays(1).dayOfWeek
+            .getDisplayName(TextStyle.FULL, Locale.ITALIAN).uppercase(Locale.ITALIAN)
+        val fiveDaysNowText = LocalDate.now().plusDays(1).dayOfWeek
+            .getDisplayName(TextStyle.FULL, Locale.ITALIAN).uppercase(Locale.ITALIAN)
+        val sixDaysNowText = LocalDate.now().plusDays(1).dayOfWeek
+            .getDisplayName(TextStyle.FULL, Locale.ITALIAN).uppercase(Locale.ITALIAN)
+        val sevenDaysNowText = LocalDate.now().plusDays(1).dayOfWeek
+            .getDisplayName(TextStyle.FULL, Locale.ITALIAN).uppercase(Locale.ITALIAN)
 
 
 
         return listOf(
 
-            DailySummaryForecast(todayDateText, dateOfYear, 22, 30,
+            DailySummaryForecast(todayDateText, dateOfMonthOne, 22, 30,
             DailySummaryForecast.weatherIcons.SUN, 0, 10),
 
-            DailySummaryForecast(tomorrowDateText, dateOfYear, 20, 28,
+            DailySummaryForecast(tomorrowDateText, dateOfMonthTwo, 20, 28,
                 DailySummaryForecast.weatherIcons.RAIN, 60, 15),
 
-            DailySummaryForecast(threeDaysNowText, dateOfYear, 23, 31,
+            DailySummaryForecast(threeDaysNowText, dateOfMonthThree, 23, 31,
             DailySummaryForecast.weatherIcons.SUN_CLOUD, 0, 12),
 
-            DailySummaryForecast(fourDaysNowText, dateOfYear, 23, 31,
+            DailySummaryForecast(fourDaysNowText, dateOfMonthFour, 23, 31,
             DailySummaryForecast.weatherIcons.SUN_CLOUD, 5, 15),
 
-            DailySummaryForecast(fiveDaysNowText, dateOfYear, 20, 28,
+            DailySummaryForecast(fiveDaysNowText, dateOfMonthFive, 20, 28,
                 DailySummaryForecast.weatherIcons.SUN_CLOUD, 0, 12),
 
-            DailySummaryForecast(sixDaysNowText, dateOfYear, 18, 24,
+            DailySummaryForecast(sixDaysNowText, dateOfMonthSix, 18, 24,
                 DailySummaryForecast.weatherIcons.RAIN, 70, 30),
 
-            DailySummaryForecast(sevenDaysNowText, dateOfYear, 23, 31,
+            DailySummaryForecast(sevenDaysNowText, dateOfMonthSeven, 23, 31,
                 DailySummaryForecast.weatherIcons.SUN_CLOUD, 0, 12)
          )
     }
