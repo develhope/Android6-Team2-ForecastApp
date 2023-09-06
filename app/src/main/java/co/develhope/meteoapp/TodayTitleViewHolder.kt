8b9ba15e1a7520ed320a94502145ddff94bcda04
Davide Francesco Riccio <ledrazors@gmail.com>
2023-09-06 17:22:19 +0200

    created title viewholder

---
+++ app/src/main/java/co/develhope/meteoapp/TodayTitleViewHolder.kt
@@ -0,0 +1,12 @@
+package co.develhope.meteoapp
+
+import androidx.recyclerview.widget.RecyclerView
+import co.develhope.meteoapp.databinding.SubtitleTodayScreenBinding
+
+class TodayTitleViewHolder(private val binding: SubtitleTodayScreenBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun onBind(item: TodayData.TodayTitle){
+        binding.todayLocationTv.text = item.todayLocation
+        binding.todaySubtitleShortTv.text = item.todayDay
+        binding.todaySubtitleLongTv.text = item.todayDate
+    }
+}
