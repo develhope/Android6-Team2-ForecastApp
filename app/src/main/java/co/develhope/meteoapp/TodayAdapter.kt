85080da40e45ce3227bdcfe66fc265484edf59f7
Davide Francesco Riccio <ledrazors@gmail.com>
2023-09-06 02:22:17 +0200

    added placeholder adapter

---
+++ app/src/main/java/co/develhope/meteoapp/TodayAdapter.kt
@@ -0,0 +1,44 @@
+package co.develhope.meteoapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import co.develhope.meteoapp.databinding.ListTodayScreenBinding
+
+class TodayAdapter(val todayList: List<TodayData>): Adapter<TodayAdapter.TodayViewHolder>(){
+
+    class TodayViewHolder(val binding: ListTodayScreenBinding): ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodayViewHolder {
+        return TodayViewHolder(ListTodayScreenBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return todayList.size
+    }
+
+    override fun onBindViewHolder(holder: TodayViewHolder, position: Int) {
+        val model = todayList[position]
+
+        holder.binding.todayTimeTv.text = model.todayTime
+        holder.binding.weatherTodayImage.setImageResource(model.todayWeatherImage)
+        holder.binding.degreesToday.text = model.todayDegrees
+        holder.binding.rainChanceImage.setImageResource(model.todayRainChanceImage)
+        holder.binding.rainChanceTv.text = model.todayRainChance
+        holder.binding.arrowImageToday.setImageResource(model.todayArrowImage)
+        holder.binding.perceivedTvToday.text = model.todayPerceivedTitle
+        holder.binding.perceivedDegreesToday.text = model.todayPerceived
+        holder.binding.uvIndexTvToday.text = model.todayUvIndexTitle
+        holder.binding.uvIndexToday.text = model.todayUvIndex
+        holder.binding.humidityTvToday.text = model.todayHumidityTitle
+        holder.binding.humidityPercentageToday.text = model.todayHumidity
+        holder.binding.windTvToday.text = model.todayWindTitle
+        holder.binding.windSpeedToday.text = model.todayWind
+        holder.binding.coverageTvToday.text = model.todayCoverageTitle
+        holder.binding.coverageToday.text = model.todayCoverage
+        holder.binding.rainTvToday.text = model.todayRainHeightTitle
+        holder.binding.rainToday.text = model.todayRainHeight
+    }
+
+}
