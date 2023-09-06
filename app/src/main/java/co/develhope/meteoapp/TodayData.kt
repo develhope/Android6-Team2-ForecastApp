3a1339e1d88e3867ea34b000ea51d13c23954ea5
Davide Francesco Riccio <ledrazors@gmail.com>
2023-09-06 02:11:09 +0200

    added placeholder data class for today items

---
+++ app/src/main/java/co/develhope/meteoapp/TodayData.kt
@@ -0,0 +1,20 @@
+package co.develhope.meteoapp
+
+data class TodayData(val todayTime: String,
+    val todayWeatherImage: Int,
+    val todayDegrees: String,
+    val todayRainChanceImage: Int,
+    val todayRainChange: String,
+    val todayArrowImage: Int,
+    val todayPerceivedTitle: String,
+    val todayPerceived: String,
+    val todayUvIndexTitle: String,
+    val todayUvIndex: String,
+    val todayHumidityTitle: String,
+    val todayHumidity: String,
+    val todayWindTitle: String,
+    val todayWind: String,
+    val todayCoverageTitle: String,
+    val todayCoverage: String,
+    val todayRainHeightTitle: String,
+    val todayRainHeight: String)
