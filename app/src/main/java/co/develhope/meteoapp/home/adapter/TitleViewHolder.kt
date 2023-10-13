7e9c4c080ef76ffd4fdf4c6f74e4d6a4c5e7d8b3
Davide Francesco Riccio <ledrazors@gmail.com>
2023-10-13 06:56:18 +0200

    Added location-change to Home Screen

--- app/src/main/java/co/develhope/meteoapp/home/adapter/TitleViewHolder.kt
+++ app/src/main/java/co/develhope/meteoapp/home/adapter/TitleViewHolder.kt
@@ -1,11 +1,13 @@
 package co.develhope.meteoapp.home.adapter
 
 import androidx.recyclerview.widget.RecyclerView
+import co.develhope.meteoapp.data.Data
 import co.develhope.meteoapp.databinding.HomeTitleBinding
 import co.develhope.meteoapp.home.WeekItems.HomeTitle
 
 class TitleViewHolder(private val binding: HomeTitleBinding) : RecyclerView.ViewHolder(binding.root) {
     fun bind(item: HomeTitle) {
-        binding.homeTitle.text = item.locality
+//        binding.homeTitle.text = item.locality
+        binding.homeTitle.text = Data.getCityLocation()
     }
 }
