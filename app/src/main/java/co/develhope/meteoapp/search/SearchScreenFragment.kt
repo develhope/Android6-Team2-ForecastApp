811bf6fbbdd9eb1640f1f0e81e00779f8f25c567
Simone Li Causi <simonelicausi96@gmail.com>
2023-10-17 22:04:55 +0200

    some adjusment

--- app/src/main/java/co/develhope/meteoapp/search/SearchScreenFragment.kt
+++ app/src/main/java/co/develhope/meteoapp/search/SearchScreenFragment.kt
@@ -3,16 +3,11 @@ package co.develhope.meteoapp.search
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
-import co.develhope.meteoapp.R
 import co.develhope.meteoapp.data.Data
 import co.develhope.meteoapp.data.local.SearchDataLocal
 import co.develhope.meteoapp.databinding.FragmentSearchScreenBinding
@@ -39,25 +34,6 @@ class SearchScreenFragment : Fragment() {
 
         val recentSearches = Data.getRecentSearches()
         binding.searchRecyclerView.adapter = DataSearchAdapter(recentSearches)
-
-
-//        // This is my search adapter.
-//        adapter = ArrayAdapter<DataSearches>(
-//            requireContext(),
-//            android.R.layout.simple_dropdown_item_1line,
-//            mutableListOf()
-//        )
-//        binding.searchEditText.setAdapter(adapter)
-
-
-        // Click listener for taking the position and save data network in a local data.
-//        binding.searchEditText.onItemClickListener =
-//            AdapterView.OnItemClickListener { p0, p1, p2, p3 ->
-//                val item: DataSearches? = adapter.getItem(p2)
-//                Data.saveSearchCity(item)
-//                findNavController().navigate(R.id.action_search_to_home_screen)
-//                Log.d("DATA SAVED", "Success: $item")
-//            }
 
 
         binding.searchEditText.addTextChangedListener(object : TextWatcher {
