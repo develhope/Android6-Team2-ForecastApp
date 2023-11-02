6c875375b7c09cbbc1ec044c43800c5ae5d967e8
Simone Li Causi <simonelicausi96@gmail.com>
2023-11-02 12:29:13 +0100

    Fix today and tomorrow bug

--- app/src/main/java/co/develhope/meteoapp/network/WeatherRepo.kt
+++ app/src/main/java/co/develhope/meteoapp/network/WeatherRepo.kt
@@ -4,34 +4,46 @@ import co.develhope.meteoapp.data.domain.DailyDataLocal
 import co.develhope.meteoapp.data.domain.WeeklyDataLocal
 import co.develhope.meteoapp.data.dto.toDailyDataLocal
 import co.develhope.meteoapp.data.dto.toWeeklyDataLocal
+import java.text.SimpleDateFormat
 import javax.inject.Inject
 
 class WeatherRepo @Inject constructor() {
-
-    private val weatherService: WeatherService = Module().getRetrofit().create(WeatherService::class.java)
+    private val weatherService: WeatherService =
+        Module().getRetrofit().create(WeatherService::class.java)
 
     private val dailyData =
         "temperature_2m,relativehumidity_2m,apparent_temperature,precipitation_probability,rain,weathercode,cloudcover,windspeed_10m,winddirection_10m,uv_index,is_day"
 
     private val weeklyData =
         "precipitation_sum,temperature_2m_max,temperature_2m_min,weathercode,windspeed_10m_max"
+
     suspend fun getWeather(
         lat: Double,
         lon: Double,
         startDate: String,
         endDate: String
     ): DailyDataLocal? {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd")
+        val startDateFormatted = dateFormat.format(dateFormat.parse(startDate))
+        val endDateFormatted = dateFormat.format(dateFormat.parse(endDate))
 
-        val response = weatherService.getDaily(lat, lon, dailyData, "UTC", startDate, endDate)
+        val response = weatherService.getDaily(
+            lat,
+            lon,
+            dailyData,
+            "UTC",
+            startDateFormatted,
+            endDateFormatted
+        )
 
         return response.toDailyDataLocal()
     }
+
     suspend fun getHomeWeather(
         lat: Double,
         lon: Double,
     ): WeeklyDataLocal? {
-
-        val response = weatherService.getWeekly(lat, lon, weeklyData,"UTC")
+        val response = weatherService.getWeekly(lat, lon, weeklyData, "UTC")
 
         return response.toWeeklyDataLocal()
     }
